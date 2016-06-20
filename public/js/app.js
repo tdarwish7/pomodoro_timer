7a8d4f524a8937b0e0bb2a6c82fc64c17da2d866
Teacher <Teacher>
2016-06-20 19:56:54 -0400

    added javascript for seconds countdown timer

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-console.log('My JavaScript is working...!!!!');
+
+$(document).ready(function(){
+  var start = $('#start');
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+
+  start.on('click', startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal = +seconds.text(); //the plus sign makes this behave like a number
+      if(secondsVal === 0){
+        seconds.text(59);
+      } else {
+        seconds.text(secondsVal - 1);
+      }
+
+    }, 1000);
+  }
+});

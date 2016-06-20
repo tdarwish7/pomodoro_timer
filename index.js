8771bfc98198eaef8b9189f566cfc5d8c2b3dfc3
Teacher <Teacher>
2016-06-20 18:46:59 -0400

    added user story number 3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = express();
 server.use(express.static(__dirname+'/public'));
 
 server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root: __dirname});
+  response.sendFile('public/html/index.html', {root:__dirname});
 });
 
 server.listen(8080);
